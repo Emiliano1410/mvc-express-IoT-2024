66a32e0e4b2b5103ec68152584078f7741fc3cb0
Emiliano1410 <a01712408@tec.mx>
2024-10-08 17:36:23 -0600

    feat: dev

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 //Para poder acceder a las variables del ambiente (.env)
 config();
 const app = express();
+console.log("hola");
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
 });
