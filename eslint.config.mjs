7c2cad1e03b6d7a47fa6d519b967da160e8b6276
Emiliano1410 <a01712408@tec.mx>
2024-10-08 17:05:52 -0600

    feat: env

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,12 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-
 export default [
-  {ignores: ["dist/"]},
-  {files: ["src/**/*.{js,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { ignores: ["dist/"] },
+  { files: ["src/**/*.{js,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export function add(a: number, b: number) {
+  const obj = {
+    hello: "world",
+  };
+
+  let c = 7;
+  return a + b;
+}
