74c19eb6d5b55b8c63dd53a8cadee83e8747388b
Emiliano1410 <a01712408@tec.mx>
2024-10-08 17:24:58 -0600

    feat: type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export function add(a: number, b: number) {
-  const obj = {
-    hello: "world",
-  };
-
-  let c = 7;
-  return a + b;
-}
+import { config } from "dotenv";
+import express from "express";
+//Para poder acceder a las variables del ambiente (.env)
+config();
+const app = express();
+app.listen(process.env.SERVER_PORT, function () {
+  console.log("Escuchando puerto " + process.env.SERVER_PORT);
+});
